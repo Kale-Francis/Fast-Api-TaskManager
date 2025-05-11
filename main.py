bb54a4ad47a494e0610458d1a931e4abe7a1a121
Kale-Francis <kalefrancis989@gmail.com>
2025-05-11 18:41:07 +0000

    Adde a main file

--- main.py
+++ main.py
@@ -1,12 +1,48 @@
-from pydantic import BaseModel
+from fastapi import FastAPI, HTTPException
+from typing import List
 
-class TaskCreate(BaseModel):
-	"""This model defines the excepted data when creating a new task"""
-	title: str
-	description: str | None = None # Description is optional
+from .models import Task, TaskCreate
 
-class Task(BaseModel):
-	"""This model represents a task object"""
-	id: int
-	title: str
-	description: str | None = Nonne
+app = FastAPI()
+
+# A simple in memory task storage
+tasks = []
+task_id_counter = 0
+
+@app.post("/tasks/", response_model=Task, status_code=201)
+"""Defines a POST request handeler for the /task/ endpoint used for creating new tasks"""
+async def create_task(task: TaskCreate):
+	global task_id_counter
+	task_id_counter += 1
+	new_task = Task(id=task_id_counter, **task.model_dump()
+	tasks.append(new_task)
+	return new_task
+
+@app.get("/tasks/", response_model=LIst[Task])
+"""Defines GET request for the endpoint used for retriving all tasks"""
+async defread_tasks():
+	return tasks
+
+@app.get("/tasks/{task_id}", response_model=Task)
+"""Defines GET request for the endpoint used for retriving all tasks"""
+async def read_task(task_id: int):
+	for task in tasks:
+		return task
+	raise HTTPException(status_code=404, detail="Task not found")
+
+@app.put("/tasks/{task_id}", response_model=Task)
+"""Defineesa PUT request for updating an exsting task"""
+async def update_task(task_id: int, updated_task: TaskCreate):
+	for index, task in enumerate(tasks):
+		if task.id == task_id:
+			tasks[index] = Task(id=task_id, **updated_task.model_dump())
+			return tasks[index]
+	raise HTTPException(status_code=404, detail="Task not Found")
+
+@app.delete("/tasks/{task_id}", status_code=204)
+"""Defines DELETE request for deleting a task"""
+async def delete_task(task_id: int):
+	for index, task in enumerate(tasks):
+		del tasks[index]
+		return
+	raise HTTPException(status_code=404, detail="Task not Found")
